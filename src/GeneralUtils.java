c182b70d072f67486e899fea25ec6fb7467b1127
fraureus <justinericardo@gmail.com>
2017-03-05 03:13:56 +0800

    partial commit: added general util and test case structure modified.

--- src/GeneralUtils.java
+++ src/GeneralUtils.java
@@ -1,20 +1,59 @@
 import java.util.Random;
 
 public class GeneralUtils {
+	private static final String BLANK = " ";
+	private static final String EMPTY_STRING = "";
+	private static final String NEW_LINE = "\n";
+	private static final String TAB = "\t";
+	
 	public int[] generateTestCase(int testSize, int maxRange, boolean includeOffset){
 		int[] testCase = new int[testSize];
 		int offset = includeOffset? 0 : 1;
 		Random rand = new Random();
 		
-		System.out.printf("generateTestCase(): Creating test cases...\n"
-				+ "\tTest Size: %d\n\tMax Value: %d\n\tOffset: %d\n", testSize, maxRange, offset);
+//		System.out.printf("generateTestCase(): Creating test cases...\n"
+//				+ "\tTest Size: %d\n\tMax Value: %d\n\tOffset: %d\n", testSize, maxRange, offset);
 		for(int i = 0; i < testCase.length; i++)
 		{
 			testCase[i] = rand.nextInt(maxRange) + offset;
 		}
 		
-		System.out.println("generateTestCase(): Done creating test cases!");
+//		System.out.println("generateTestCase(): Done creating test cases!\n");
 		
 		return testCase;
+	}
+	
+	public String prettifyOutput(int[] testResult, int maxOutput){
+		StringBuilder pwettyOutput = new StringBuilder(EMPTY_STRING);
+		
+		for(int i = 0; i < testResult.length; i++){
+			if(i == maxOutput){
+				break;
+			}
+			
+			pwettyOutput.append(testResult[i] + TAB);
+			if((i + 1) % 10 == 0){
+				pwettyOutput.append(NEW_LINE);
+			}
+		}
+		
+		return isNotNullOrEmpty(pwettyOutput.toString()) ? pwettyOutput.toString() : EMPTY_STRING;
+	}
+	
+	public String prettifyOutput(int[] testResult){
+		return prettifyOutput(testResult, -1);
+	}
+	
+	public boolean isNotNullOrEmpty(String testString){
+		
+		if(testString.length() > 0 && testString != null){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public long getTimeDifference(long startTime){
+		return System.currentTimeMillis() - startTime;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,39 @@
-import sortlib.MergeSort;
+import sortlib.SequentialMergeSort;
 
 public class Main {
 
+	private static final String BLANK = " ";
+	private static final int[] defaultTestCases = {50000, 100000, 500000, 1000000, 5000000};
+	private static GeneralUtils util;
+	private static SequentialMergeSort seqMerge;
+	
+	// sample use case
 	public static void main(String[] args) {
-		GeneralUtils util = new GeneralUtils();
-		util.generateTestCase(10000, 2000, false);
+		util = new GeneralUtils();
+		seqMerge = new SequentialMergeSort();
+		
+		int testCaseNum = 0;
+		for(int test: defaultTestCases){
+			runTestCase(test, 2000, false, "Sequential Test Case " + testCaseNum);
+			testCaseNum++;
+		}
+	}
+	
+	public static void runTestCase(int testSize, int testRange, boolean testOffset, String testCaseName){
+		long startTime =  0L, runTime = 0L;
+		int[] testCase = util.generateTestCase(testSize, testRange, testOffset);
+		
+		try {
+			startTime = System.currentTimeMillis();
+			seqMerge.sort(testCase);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			runTime = util.getTimeDifference(startTime);
+			System.out.printf("Execution Time of %s: %s milliseconds(s)\n", testCaseName, runTime);
+		}
+		
+//		System.out.println("Output:\n" + util.prettifyOutput(testCase, 10));
 	}
 
 }
